1f9dc6b6e22862db367cca249a4ad5fd7d2e5788
Mehdi8088sohrabi <mehdi.s.m13880103@gmail.com>
2024-07-12 22:14:35 +0430

    Mini-project4-Digikala-input-panel

---
+++ index.js
@@ -0,0 +1,30 @@
+let myfocus = document.getElementById('myinput');
+let defu = document.getElementById('defu');
+let button = document.getElementById('button');
+
+
+myfocus.addEventListener('focus', function() {
+    defu.classList.add('hidden');
+});
+
+
+myfocus.addEventListener('blur', function() {
+    if (myfocus.value === '') {
+        defu.classList.remove('hidden');
+        defu.innerHTML = 'لطفا این فیلد را پر کنید';
+    } else if (myfocus.value.length !== 11) {
+        defu.classList.remove('hidden');
+        defu.innerHTML = 'این شماره نامعتبر است';
+    } else {
+        defu.classList.add('hidden');
+    }
+});
+
+
+button.addEventListener('click', function() {
+    if (myfocus.value.length === 11) {
+        window.location.href = 'https://www.digikala.com/users/login/?backUrl=/';
+    } else {
+        alert('شماره وارد شده باید دقیقا 11 کاراکتر باشد.');
+    }
+});
